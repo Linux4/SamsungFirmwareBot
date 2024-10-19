25302933d3212674eaf6979bd4d739a2989b476c
Tim Zimmermann <tim@linux4.de>
2024-10-19 06:10:45 +0200

    Update dependencies

--- src/main/java/de/linux4/samsungfwbot/SamsungFWBot.java
+++ src/main/java/de/linux4/samsungfwbot/SamsungFWBot.java
@@ -30,20 +30,20 @@ import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
 import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.groupadministration.SetChatDescription;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Enumeration;
@@ -55,14 +55,14 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-public class SamsungFWBot extends TelegramLongPollingBot {
+public class SamsungFWBot implements LongPollingSingleThreadUpdateConsumer{
 
     public static final String KERNEL_REPO_URL = "https://github.com/Linux4/samsung_kernel";
     public static final String GH_USER = "Linux4";
     public static final int MAX_CONCURRENT_DOWNLOADS = 2;
 
     public static void main(String[] args) {
-        if (args.length != 5 && args.length != 6) {
+        if (args.length != 4 && args.length != 5) {
             if (args.length == 1 && args[0].equalsIgnoreCase("scrapeDevices")) {
                 try {
                     SamsungDeviceScraper.main(args);
@@ -71,19 +71,19 @@ public class SamsungFWBot extends TelegramLongPollingBot {
                 return;
             }
             // channels can be id or @channelname
-            System.out.println("Usage: java -jar samsungfwbot.jar <bot name> <bot token> <capsolver token> <firmware channel> <kernel channel> [oneshot]");
+            System.out.println("Usage: java -jar samsungfwbot.jar <bot token> <capsolver token> <firmware channel> <kernel channel> [oneshot]");
             System.out.println("Usage: java -jar samsungfwbot.jar scrapeDevices");
-            return;
+            System.exit(1);
         }
 
-        boolean oneshot = args.length == 6 && args[5].equalsIgnoreCase("oneshot");
+        boolean oneshot = args.length == 5 && args[4].equalsIgnoreCase("oneshot");
 
         try {
-            TelegramBotsApi botApi = new TelegramBotsApi(DefaultBotSession.class);
-            SamsungFWBot bot = new SamsungFWBot(args[0], args[1], args[2], args[3], args[4], oneshot);
-            botApi.registerBot(bot);
+            TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+            SamsungFWBot bot = new SamsungFWBot(args[0], args[1], args[2], args[3], oneshot);
+            botsApplication.registerBot(args[0], bot);
             bot.run();
-        } catch (TelegramApiException | IOException | ParseException ex) {
+        } catch (TelegramApiException ex) {
             ex.printStackTrace();
         }
     }
@@ -96,25 +96,21 @@ public class SamsungFWBot extends TelegramLongPollingBot {
         }
     }
 
-    private final String botName;
-    private final String token;
     private final CapSolver capSolver;
     private final String channelFw;
     private final String channelKernel;
     private final boolean oneshot;
+    private final TelegramClient telegramClient;
     private boolean checksFinished = false;
     private final ConcurrentLinkedQueue<TelegramMessage> messageQueue = new ConcurrentLinkedQueue<>();
 
-    public SamsungFWBot(String botName, String token, String capSolverToken, String channelFw, String channelKernel,
-                        boolean oneshot) throws TelegramApiException, IOException, ParseException {
-        super(new DefaultBotOptions());
-
-        this.botName = botName;
-        this.token = token;
+    public SamsungFWBot(String botToken, String capSolverToken, String channelFw, String channelKernel, boolean oneshot) {
         this.capSolver = new CapSolver(capSolverToken);
         this.channelFw = channelFw;
         this.channelKernel = channelKernel;
         this.oneshot = oneshot;
+
+        this.telegramClient = new OkHttpTelegramClient(botToken);
     }
 
     public void run() {
@@ -135,18 +131,18 @@ public class SamsungFWBot extends TelegramLongPollingBot {
                 if (!messageQueue.isEmpty()) {
                     TelegramMessage message = messageQueue.poll();
 
-                    SendMessage sm = new SendMessage();
+                    // Telegram message length limit: 4096
+                    SendMessage sm = new SendMessage(message.getChannelId(),
+                            message.getText().substring(0, Math.min(message.getText().length(), 4096)));
                     sm.setChatId(message.getChannelId());
                     //sm.setParseMode("HTML");
-                    // Telegram message length limit: 4096
-                    sm.setText(message.getText().substring(0, Math.min(message.getText().length(), 4096)));
 
                     if (message.getKeyboard() != null)
                         sm.setReplyMarkup(message.getKeyboard());
 
                     for (int i = 0; i < 5; i++) {
                         try {
-                            execute(sm);
+                            telegramClient.execute(sm);
                             break;
                         } catch (TelegramApiException e) {
                             e.printStackTrace();
@@ -177,11 +173,10 @@ public class SamsungFWBot extends TelegramLongPollingBot {
                             found = true;
 
                             if (info.isNewerThan(db.getPDA(model))) {
-                                InlineKeyboardMarkup.InlineKeyboardMarkupBuilder keyboardBuilder =
+                                InlineKeyboardMarkup keyboard =
                                         InlineKeyboardMarkup.builder().keyboardRow(
-                                                List.of(InlineKeyboardButton.builder().text("Download")
-                                                        .url(info.getDownloadURL()).build()));
-                                InlineKeyboardMarkup keyboard = keyboardBuilder.build();
+                                                new InlineKeyboardRow(InlineKeyboardButton.builder().text("Download")
+                                                        .url(info.getDownloadURL()).build())).build();
 
                                 messageQueue.add(new TelegramMessage(channelFw, "New firmware update available \n \n"
                                         + "Device: " + info.getDeviceName() + " \n"
@@ -294,11 +289,10 @@ public class SamsungFWBot extends TelegramLongPollingBot {
                                             push.setCredentialsProvider(new UsernamePasswordCredentialsProvider(GH_USER, System.getenv("GH_TOKEN")));
                                             push.call();
 
-                                            InlineKeyboardMarkup.InlineKeyboardMarkupBuilder keyboardBuilder =
+                                            InlineKeyboardMarkup keyboard =
                                                     InlineKeyboardMarkup.builder().keyboardRow(
-                                                            List.of(InlineKeyboardButton.builder().text("View")
-                                                                    .url(KERNEL_REPO_URL + "/tree/" + model + '/' + info.getPDA()).build()));
-                                            InlineKeyboardMarkup keyboard = keyboardBuilder.build();
+                                                            new InlineKeyboardRow(InlineKeyboardButton.builder().text("View")
+                                                                    .url(KERNEL_REPO_URL + "/tree/" + model + '/' + info.getPDA()).build())).build();
                                             messageQueue.add(new TelegramMessage(channelKernel, "New kernel sources available! \n"
                                                     + "Model: " + info.getModel() + " \n"
                                                     + "PDA Version: " + info.getPDA() + " \n"
@@ -336,12 +330,12 @@ public class SamsungFWBot extends TelegramLongPollingBot {
             }
 
             try {
-                SetChatDescription sdesc = new SetChatDescription();
-                sdesc.setDescription("Last updated: " + new Date(System.currentTimeMillis()));
+                SetChatDescription sdesc = new SetChatDescription(channelFw, "Last updated: "
+                        + new Date(System.currentTimeMillis()));
                 sdesc.setChatId(channelFw);
-                execute(sdesc);
+                telegramClient.execute(sdesc);
                 sdesc.setChatId(channelKernel);
-                execute(sdesc);
+                telegramClient.execute(sdesc);
             } catch (TelegramApiException ex) {
                 ex.printStackTrace();
             }
@@ -399,16 +393,6 @@ public class SamsungFWBot extends TelegramLongPollingBot {
     }
 
     @Override
-    public String getBotUsername() {
-        return botName;
-    }
-
-    @Override
-    public String getBotToken() {
-        return token;
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
+    public void consume(Update update) {
     }
 }
